863d26ade177385222b26ed4b38e49c247849aaa
lembolov9 <Tomas080808>
2019-04-17 18:28:55 +0300

    changes with migrations and add requirements.txt

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ class LogMixin(LoginRequiredMixin):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['user'] = self.request.user.pk
+        context['user'] = self.request.user
         return context
 
 class PostDetailView(LogMixin, DetailView):
